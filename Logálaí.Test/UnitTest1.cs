0a7a0e70fe7105cbda2732d2e977ccf856d28f54
James <118912301+jclar0@users.noreply.github.com>
2023-12-30 09:49:35 +0000

    Update tests for new version

--- Logálaí.Test/UnitTest1.cs
+++ Logálaí.Test/UnitTest1.cs
@@ -7,20 +7,54 @@ namespace Logálaí.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestError()
         {
             // Arrange
-            using (StringWriter sw = new StringWriter())
-            {
-                Console.SetOut(sw);
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
 
-                // Act
-                Log.Info("Hello, world!");
+            Log instance = new Log();
 
-                // Assert
-                string output = sw.ToString();
-                Assert.IsTrue(output.Contains("Hello, world!"));
-            }
+            // Act
+            instance.Error("This is an error test!");
+
+            // Assert
+            string output = sw.ToString();
+            Assert.IsTrue(output.Contains("[ERROR] This is an error test!"));
+        }
+
+        [TestMethod]
+        public void TestWarning()
+        {
+            // Arrange
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            Log instance = new Log();
+
+            // Act
+            instance.Warning("This is a warning test!");
+
+            // Assert
+            string output = sw.ToString();
+            Assert.IsTrue(output.Contains("[WARNING] This is a warning test!"));
+        }
+
+        [TestMethod]
+        public void TestInfo()
+        {
+            // Arrange
+            StringWriter sw = new StringWriter();
+            Console.SetOut(sw);
+
+            Log instance = new Log();
+
+            // Act
+            instance.Info("This is an info test!");
+
+            // Assert
+            string output = sw.ToString();
+            Assert.IsTrue(output.Contains("[INFO] This is an info test!"));
         }
     }
 }
